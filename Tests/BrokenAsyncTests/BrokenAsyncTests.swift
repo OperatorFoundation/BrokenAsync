54db3a8673d68362e470eeebe56f0a5fc8aec928
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-07-18 17:00:24 -0500

    Added timeout for expectation wait

--- Tests/BrokenAsyncTests/BrokenAsyncTests.swift
+++ Tests/BrokenAsyncTests/BrokenAsyncTests.swift
@@ -24,6 +24,6 @@ final class BrokenAsyncTests: XCTestCase {
             task2Finished.fulfill()
         }
 
-        wait(for: [task1Entered, task2Finished])
+        wait(for: [task1Entered, task2Finished], timeout: 60)
     }
 }
